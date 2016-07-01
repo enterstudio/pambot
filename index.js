04719af74a681a247159a4f6c54e13f0f664b9bd
Daniel Matteson <dmatt@disqus.com>
2016-07-01 03:35:30 -0700

    add request verification

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
-const express = require('express');
+const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const crypto = require('crypto');
 const app = express()
 const token = process.env.FB_PAGE_ACCESS_TOKEN
-// Wit.ai parameters
+const FB_APP_SECRET = process.env.FB_APP_SECRET;
 const WIT_TOKEN = process.env.WIT_TOKEN;
 
 app.set('port', (process.env.PORT || 5000))
@@ -14,7 +15,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(bodyParser.urlencoded({extended: false}))
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json({ verify: verifyRequestSignature }));
 
 // Index route
 app.get('/', function (req, res) {
@@ -128,56 +129,33 @@ function sendGenericMessage(sender) {
 }
 
 // wit actions 
-let Wit = null;
-try {
-  // if running from repo
-  Wit = require('../').Wit;
-} catch (e) {
-  Wit = require('node-wit').Wit;
+
+/*
+ * Verify that the callback came from Facebook. Using the App Secret from
+ * the App Dashboard, we can verify the signature that is sent with each
+ * callback in the x-hub-signature field, located in the header.
+ *
+ * https://developers.facebook.com/docs/graph-api/webhooks#setup
+ *
+ */
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', FB_APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
 }
-
-const accessToken = (() => {
-  if (process.argv.length !== 3) {
-    console.log('usage: node examples/quickstart.js <wit-access-token>');
-    process.exit(1);
-  }
-  return process.argv[2];
-})();
-
-// Quickstart example
-// See https://wit.ai/ar7hur/quickstart
-
-const firstEntityValue = (entities, entity) => {
-  const val = entities && entities[entity] &&
-    Array.isArray(entities[entity]) &&
-    entities[entity].length > 0 &&
-    entities[entity][0].value
-  ;
-  if (!val) {
-    return null;
-  }
-  return typeof val === 'object' ? val.value : val;
-};
-
-const actions = {
-  send(request, response) {
-    const {sessionId, context, entities} = request;
-    const {text, quickreplies} = response;
-    return new Promise(function(resolve, reject) {
-      console.log('sending...', JSON.stringify(response));
-      return resolve();
-    });
-  },
-  recDiscussion({context, entities}) {
-    return new Promise(function(resolve, reject) {
-      var topic = firstEntityValue(entities, 'topic')
-      if (topic) {
-        context.comment = 'this is a relevant comment about' + topic; // we should call a weather API here
-      }
-      return resolve(context);
-    });
-  },
-};
-
-const client = new Wit({accessToken, actions});
-client.interactive();
